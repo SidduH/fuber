07899624ec84f5d4fe7be73f9747b1481e32d2e9
siddu <siddaram.halli@gmail.com>
2016-08-20 20:13:26 +0530

    added db files

--- taxi_service/models.py
+++ taxi_service/models.py
@@ -1,25 +1,30 @@
 from django.db import models
 
-
 class Cabs(models.Model):
     cab_id = models.IntegerField(primary_key=True)
     owner_name = models.CharField(max_length=100)
-    lattitude = models.FloatField()
-    longitude = models.FloatField()
-    status = models.BooleanField()
-    cab_type = models.CharField()
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+    status = models.CharField(max_length=50)  # free or booked
+    cab_type = models.CharField(max_length=100)  # Pink or not
+
+    def __str__(self):
+        return self.owner_name
 
 
 class Customer(models.Model):
     cust_id = models.IntegerField(primary_key=True)
     cust_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.cust_name
+
 
 class Rides(models.Model):
-    cab_id = models.ForeignKey(Cabs)
-    cust_id = models.ForeignKey(Customer)
-    start_lattitude = models.FloatField()
+    cab_id = models.ForeignKey(Cabs, on_delete=models.CASCADE)
+    cust_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    start_latitude = models.FloatField()
     start_longitude = models.FloatField()
-    stop_lattitude = models.FloatField()
+    stop_latitude = models.FloatField()
     stop_longitude = models.FloatField()
 
